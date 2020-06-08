4471a2a24d001e2a78720c1252fab0391331e087
RoParadiso <66187087+RoParadiso@users.noreply.github.com>
2020-06-08 04:22:03 -0300

    Add files via upload

---
+++ If-04.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main (){
+
+	char lluvia = s;
+	char paraguas = s;
+
+	printf("¿Esta lloviendo?\nIngresa [s] para si y [n] para no\n");
+	scanf(" %c", &lluvia);
+
+	if (lluvia = s)
+	{
+		printf("¿Tenes el paraguas a mano?\nIngresa [s] para si y [n] para no\n");
+		scanf(" %c", &paraguas);
+	}
+			if (paraguas = s)
+		{
+			printf("Está lloviendo, no lo olvides.");
+		}
+		if else
+		{
+			printf("Está lloviendo, a correr");
+		}
+	if else
+	{
+		printf("No llueve.");
+	}
+
+	return 0;
+}
